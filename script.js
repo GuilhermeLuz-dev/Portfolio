bc07129ca3c1c7da8e5f49bf674190e31c80780d
Guilherme Luz Rocha <luzguilherme.dev@gmail.com>
2022-12-02 11:57:26 -0300

    Criando seção sobreMim

---
+++ script.js
@@ -0,0 +1,47 @@
+let slide1 =  document.querySelector(".slide1");
+let slide2 =  document.querySelector(".slide2");
+let slide3 =  document.querySelector(".slide3");
+let s1 = document.querySelector(".s1");
+let s2 = document.querySelector(".s2");
+let s3 = document.querySelector(".s3");
+let biografia = document.querySelector(".biografia");
+let sobreMim = document.querySelector(".sobreMim");
+let imgBio = document.querySelector(".imgBio");
+
+const efeitoSlide1 = ()=>{
+    console.log("teste");
+    s1.style.marginLeft = "0";
+    s1.style.backgroundColor = "#027fe9";
+    biografia.style.backgroundColor = "#142246";
+    sobreMim.style.backgroundColor = "#1c3166";
+    imgBio.setAttribute("src", "./images/livro.gif");
+    slide1.style.backgroundColor = "#fff";
+    slide2.style.backgroundColor = "transparent";
+    slide3.style.backgroundColor = "transparent";
+}
+const efeitoSlide2 = ()=>{
+    console.log("teste");
+    s1.style.marginLeft = "-32%";
+    s2.style.backgroundColor = "#800909";
+    biografia.style.backgroundColor = "#4d1a1a";
+    sobreMim.style.backgroundColor = "#332222";
+    imgBio.setAttribute("src", "./images/monitor.gif");
+    slide1.style.backgroundColor = "transparent";
+    slide2.style.backgroundColor = "#fff";
+    slide3.style.backgroundColor = "transparent";
+}
+const efeitoSlide3 = ()=>{
+    console.log("teste");
+    s1.style.marginLeft ="-64%";
+    s3.style.backgroundColor = "#4f784e";
+    biografia.style.backgroundColor = "#2d5e4c";
+    sobreMim.style.backgroundColor = "#13444d";
+    imgBio.setAttribute("src", "./images/cara.gif");
+    slide1.style.backgroundColor = "transparent";
+    slide2.style.backgroundColor = "transparent";
+    slide3.style.backgroundColor = "#fff";
+}
+
+slide1.addEventListener("click", efeitoSlide1);
+slide2.addEventListener("click", efeitoSlide2);
+slide3.addEventListener("click", efeitoSlide3);
