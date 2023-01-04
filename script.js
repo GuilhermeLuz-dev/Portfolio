a4dd313c6eae5e67bf2f5f7787a5e1d5966ec468
Guilherme Luz Rocha <luzguilherme.dev@gmail.com>
2023-01-04 16:54:26 -0300

    Atualização
    Renomeando algumas classe e adicionando comentários.

--- script.js
+++ script.js
@@ -1,90 +1,94 @@
-let slide1 = document.querySelector(".slide1");
-let slide2 = document.querySelector(".slide2");
-let slide3 = document.querySelector(".slide3");
-let s1 = document.querySelector(".s1");
-let s2 = document.querySelector(".s2");
-let s3 = document.querySelector(".s3");
-let biografia = document.querySelector(".biografia");
-let sobreMim = document.querySelector(".sobreMim");
-let imgBio = document.querySelector(".imgBio");
+// Constantes para efeito de passagem de slide na seção biografia.
+const slide1 = document.querySelector(".slide1");
+const slide2 = document.querySelector(".slide2");
+const slide3 = document.querySelector(".slide3");
+const s1 = document.querySelector(".s1");
+const s2 = document.querySelector(".s2");
+const s3 = document.querySelector(".s3");
+const textos = document.querySelector(".textos");
+const biografia = document.querySelector(".biografia");
+const imgBio = document.querySelector(".imgBio");
 
+// Constantes para efeito de carrossel na seção de Projetos.
 const controls = document.querySelectorAll('.control');
 const projeto = document.querySelectorAll('.projeto');
-const card = document.querySelectorAll('.card')
+const card = document.querySelectorAll('.card');
 
-let descricao = document.querySelectorAll(".descricao");
+// Contatnte para descrições.
+const descricao = document.querySelectorAll(".descricao");
 
+// Contantes para botão de contato fixo
 const contato = document.querySelector(".contato")
 const imgContato = document.querySelectorAll(".imgContato")
 
 
-
+// Funções para efeito de passagem de slide da seção Biografia.
 const efeitoSlide1 = () => {
-  console.log("teste");
   s1.style.marginLeft = "0";
   s1.style.backgroundColor = "#027fe9";
-  biografia.style.backgroundColor = "#142246";
-  sobreMim.style.backgroundColor = "#1c3166";
+  textos.style.backgroundColor = "#142246";
+  biografia.style.backgroundColor = "#1c3166";
   imgBio.setAttribute("src", "./images/livro.gif");
   slide1.style.backgroundColor = "#fff";
   slide2.style.backgroundColor = "transparent";
   slide3.style.backgroundColor = "transparent";
 };
 const efeitoSlide2 = () => {
-  console.log("teste");
   s1.style.marginLeft = "-34%";
   s2.style.backgroundColor = "#800909";
-  biografia.style.backgroundColor = "#4d1a1a";
-  sobreMim.style.backgroundColor = "#332222";
+  textos.style.backgroundColor = "#4d1a1a";
+  biografia.style.backgroundColor = "#332222";
   imgBio.setAttribute("src", "./images/monitor.gif");
   slide1.style.backgroundColor = "transparent";
   slide2.style.backgroundColor = "#fff";
   slide3.style.backgroundColor = "transparent";
 };
 const efeitoSlide3 = () => {
-  console.log("teste");
   s1.style.marginLeft = "-68%";
   s3.style.backgroundColor = "#4f784e";
-  biografia.style.backgroundColor = "#2d5e4c";
-  sobreMim.style.backgroundColor = "#13444d";
+  textos.style.backgroundColor = "#2d5e4c";
+  biografia.style.backgroundColor = "#13444d";
   imgBio.setAttribute("src", "./images/cara.gif");
   slide1.style.backgroundColor = "transparent";
   slide2.style.backgroundColor = "transparent";
   slide3.style.backgroundColor = "#fff";
 };
 
+// Adicionando eventos aos controles que indicam o slide atual da seção Biografia.
 slide1.addEventListener("click", efeitoSlide1);
 slide2.addEventListener("click", efeitoSlide2);
 slide3.addEventListener("click", efeitoSlide3);
 
-let margin = 0; 
+// Adicionando efeito de carrossel na seção Projetos.
+
+// Variáveis auxiliares
+let margin = 0;
 let index = 1;
-controls.forEach(control =>{
-  let isleft = control.classList.contains('arrow-left');
-  control.addEventListener("click", ()=>{
-    if(isleft){
-      if(margin >= 0){
-        projeto.forEach(projeto =>{
+controls.forEach(control => {
+  let isleft = control.classList.contains('arrow-left'); //Verificando se o controle clicado é o esquerdo.
+  control.addEventListener("click", () => { //Adicionando evento de click aos controles.
+    if (isleft) {
+      if (margin >= 0) { //Adicionando animação caso o limite de projetos seja atingido.
+        projeto.forEach(projeto => {
           projeto.style.animation = "limited1 0.5s";
-          setTimeout(()=>{
+          setTimeout(() => {
             projeto.style.animation = "none";
-          }, 500)
+          }, 500)//Retirando animação para evitar bugs.
         })
-     
-      }else{
+
+      } else { //Caso o limite não tenha exedido, é adicionando margin ao primeiro elemento para criar o efeito de carrossel.
         margin += 335
-        console.log(margin)
         projeto[0].style.marginLeft = `${margin}px`;
       }
-    }else{
-      if(margin <= -670){
-        projeto.forEach(projeto =>{
+    } else { //Caso o botão direito tenha sido clicado é retirado margin para o efeito de retorno. 
+      if (margin <= -670) {
+        projeto.forEach(projeto => { //Animação caso chegue ao limite de projetos.
           projeto.style.animation = "limited2 0.5s";
-          setTimeout(()=>{
+          setTimeout(() => {
             projeto.style.animation = "none";
           }, 500)
         })
-      }else{
+      } else {
         margin += -335;
         console.log(margin)
         projeto[0].style.marginLeft = `${margin}px`;
@@ -94,38 +98,35 @@ controls.forEach(control =>{
   })
 })
 
-card.forEach(card =>{
-  card.addEventListener("mouseover",()=>{
+// Adicionado uma descrição aos projetos ao passar o mause no mesmo.
+card.forEach(card => {
+  card.addEventListener("mouseover", () => {
     card.lastElementChild.style.bottom = "0";
   })
 })
-card.forEach(card =>{
-  card.addEventListener("mouseout",()=>{
+card.forEach(card => {
+  card.addEventListener("mouseout", () => {
     card.lastElementChild.style.bottom = "-90px";
   })
 })
 
+// Criando efeito no botão fixo onde toda forma de contato pode ser acessada facilmente.
 let aux = 95;
-const aparecerRedes = ()=>{
-  for(let i = 0 ; i < imgContato.length; i++){
+const aparecerRedes = () => {
+  for (let i = 0; i < imgContato.length; i++) {
     imgContato[i].style.opacity = '1';
     imgContato[i].style.bottom = `${aux}px`;
     aux += 95;
   }
   contato.setAttribute('onclick', 'desaparecerRedes()')
-
-  
 }
-
-
-const desaparecerRedes = ()=>{
-  
-  for(let i = 0 ; i < imgContato.length; i++){
+const desaparecerRedes = () => {
+  for (let i = 0; i < imgContato.length; i++) {
     imgContato[i].style.opacity = "0"
     imgContato[i].style.bottom = "15px";
     aux = 95;
-    
   }
+  contato.setAttribute('onclick', 'aparecerRedes()')
 }
 
 
